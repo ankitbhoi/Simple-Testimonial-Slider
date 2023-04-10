0573f62e9025d0b1a40f76173eb9c2da889ae6e2
ankitbhoi <ankitghosal2002@gmail.com>
2023-04-10 13:05:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Testimonial from "./components/Testimonial"
 import reviews from "./data";
 const App = () => {
   return(
-    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-orange-900 via-amber-100 to-orange-900">
       <div className="text-center">
         <Header/>
         <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
